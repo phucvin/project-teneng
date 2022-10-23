e002422c69abb491ebef600965a6202cd9ec26e3
phucvin <phucvin@gmail.com>
2022-10-23 01:58:28 +0000

    Add ServiceRouter

---
+++ services/servicerouter/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "github.com/phucvin/project-teneng/services/servicerouter/servicerouter"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+// server is used to implement servicerouter.ServiceRouterServer.
+type server struct {
+	pb.UnimplementedServiceRouterServer
+}
+
+// Invoke implements servicerouter.ServiceRouterServer
+func (s *server) Invoke(ctx context.Context, in *pb.InvokeRequest) (*pb.InvokeResponse, error) {
+	log.Printf("Received: %v", in.GetDescription())
+	return &pb.InvokeResponse{Description: "Received " + in.GetDescription()}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterServiceRouterServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
---
+++ services/servicerouter/proto/go.mod
@@ -0,0 +1,3 @@
+module github.com/phucvin/project-teneng/services/servicerouter/proto
+
+go 1.16
